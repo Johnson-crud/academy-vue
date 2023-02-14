c432530ec02240e309e1cf5b0c1cc720ab4aba97
Johnson-crud <117722753+Johnson-crud@users.noreply.github.com>
2023-02-14 16:06:02 +0530

    tailwind_Added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { store } from './store'
+import './index.css'
 import App from './App.vue'
 import router from "./router/index"
 import { plugin, defaultConfig } from '@formkit/vue'

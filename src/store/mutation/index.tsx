c4e2e3c3d9208f38fcc3a129135e4290c59bd5e4
Johnson-crud <117722753+Johnson-crud@users.noreply.github.com>
2023-02-14 11:18:37 +0530

    small

--- src/store/mutation/index.tsx
+++ src/store/mutation/index.tsx
@@ -1,23 +1,5 @@
-interface InitialValue{
-    Users:Array<any>,
-    Branch:Array<any>,
-    Batch:Array<any>
+const increment=()=>{
+    return 
 }
-// type data={
-//     name:string,
-//     emailId:string,
-//     contactNo:number | string,
-// }
-// type data1={
-//     branch_name:string,
-//     des
-// }
 
-const Users:InitialValue[]=[]
-const Branch:InitialValue[]=[]
-const Batch:InitialValue[]=[]
-
-
-
-
-export default { Users,Branch,Batch }
+export {increment} 
